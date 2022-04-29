033be52776f7d47cd44a3f97e3d4ffb21b4dd292
Danny <dannydwwagner@gmail.com>
2022-04-29 12:04:01 +0200

    added hypertext link on index directing to the list by alphabetical order

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,8 @@
 
 @section('title', 'Liste des tâches')
 
+<a href="/index2" style="margin:20px; text-decoration: underline;">Index par ordre alphabétique</a>
+
 @section('main')
 	<div class="task-list">
 		@foreach($tasks as $task)
